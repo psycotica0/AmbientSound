8125c5f6df88811239cb08554f966376edb43387
Christopher Vollick <psycotica0@gmail.com>
2010-01-30 16:21:52 -0500

    User Pregenerated Tones.
    Now instead of doing sin functions on the fly the math is all done before hand.
    
    This gives me a set of 'Tones' that my intruments can choose from.
    
    All in all, I think this makes whole thing a lot cleaner, and also allows for more complicated tones later.

--- chords.c
+++ chords.c
@@ -22,11 +22,18 @@
 #define MAX_VOLUME 127
 #define MIN_VOLUME 0
 
+/* This structure represents a tone */
+typedef struct Tone {
+	char* name; /* This is a string name of the tone, like C# */
+	float freq; /* This is an integer that is the frequency of the tone */
+	int period; /* This is the period of the tone, in 'ticks' */
+	float* sample; /* This is an array of length 'period' that is the value of this wave at each 'tick' */
+} Tone;
+
 /* This holds each instrument */
-typedef struct instrument {
-	float freqRate; /* This represents the frequency, it is what the position means. 2*PI*(freq/sampleFreq) */
+typedef struct Instrument {
+	Tone* tone;
 	long position; /* This is where in the note's wave we are measured in 'ticks' */
-	int period; /* This is the period of the wave measured in 'ticks' */
 
 	/* The arrangment of notes is done in terms of beats */
 	/* At the end of a beat, the song doesn't really move forward a beat, it's just that every event moves closer a beat */
@@ -40,7 +47,27 @@ typedef struct instrument {
 	int end_beat;
 
 	int volume; /* This is what the volume of the note is */
-} instrument;
+} Instrument;
+
+/* This is the global registry of all Tones available */
+typedef struct Tones {
+	Tone* tone; /* Array of precomputed tones */
+	int num; /* Number of precomputed tones in array */
+} Tones;
+
+/* This is the global registery of all instruments */
+typedef struct Instruments {
+	Instrument* instrument; /* Array of instruments */
+	int num; /* Number of instruments in array */
+} Instruments;
+
+/* This is the struture that represents all data in this arrangement. */
+typedef struct GlobalData {
+	Instruments instruments; /* This references all the instruments playing in this piece. */
+	Tones tones; /* This refrences all the tones those instruments can play. */
+	long beat_position; /* This is where in a beat we've played to thus far, measured in 'ticks'*/
+	long beat_length; /* This is how long each beat is, measured in 'ticks' */
+} GlobalData;
 
 float freqToFreqRate(float freq) {
 	return ((2 * M_PI * freq) / FREQUENCY);
@@ -50,69 +77,40 @@ float freqToPeriod(float freq) {
 	return (FREQUENCY / freq);
 }
 
-/* This function takes in the current instrument and makes the next tone for it to play */
-void generateTone(instrument* currentInstrument) {
-	const static float freqs[] = {
-		65.41, /* C2 */
-		69.30, /* C#2/Db2 */
-		73.42, /* D2 */
-		77.78, /* D#2/Eb2 */
-		82.41, /* E2 */
-		87.31, /* F2 */
-		92.50, /* F#2/Gb2 */
-		98.00, /* G2 */
-		103.83, /* G#2/Ab2 */
-		110.00, /* A2 */
-		116.54, /* A#2/Bb2 */
-		123.47, /* B2 */
-		130.81, /* C3 */
-		138.59, /* C#3/Db3 */
-		146.83, /* D3 */
-		155.56, /* D#3/Eb3 */
-		164.81, /* E3 */
-		174.61, /* F3 */
-		185.00, /* F#3/Gb3 */
-		196.00, /* G3 */
-		207.65, /* G#3/Ab3 */
-		220.00, /* A3 */
-		233.08, /* A#3/Bb3 */
-		246.94, /* B3 */
-		261.63, /* C4 */
-		277.18, /* C#4/Db4 */
-		293.66, /* D4 */
-		311.13, /* D#4/Eb4 */
-		329.63, /* E4 */
-		349.23, /* F4 */
-		369.99, /* F#4/Gb4 */
-		392.00, /* G4 */
-		415.30, /* G#4/Ab4 */
-		440.00, /* A4 */
-		466.16, /* A#4/Bb4 */
-		493.88, /* B4 */
-		523.25, /* C5 */
-	};
-	const static int numFreqs = sizeof(freqs) / sizeof(float);
-	float tempFreq;
+/* This function computes the data for a given tone */
+void genTone(Tone* tone, float freq, char* name) {
+	int i;
+	float freqRate = freqToFreqRate(freq);
 
+	tone->freq = freq;
+	tone->name = name;
+	tone->period = freqToPeriod(freq);
+
+	tone->sample = malloc(sizeof(float) * tone->period);
+	for (i=0; i<=tone->period; i++) {
+		tone->sample[i] = sinf(i * freqRate);
+	}
+}
+
+/* This function takes in the current instrument and makes the next tone for it to play */
+void nextNote(Instrument* currentInstrument, Tones* tones) {
+	currentInstrument->tone = tones->tone + (random() % tones->num);
 	currentInstrument->start_beat = (random() % (UPPER_START_BEAT - LOWER_START_BEAT)) + LOWER_START_BEAT;
 	currentInstrument->end_beat = currentInstrument->start_beat + (random() % (UPPER_DURATION - LOWER_DURATION)) + LOWER_DURATION;
 	currentInstrument->volume = MAX_VOLUME;
-	/* Generate Random Frequency and Period */
-	tempFreq = freqs[random() % numFreqs];
-	currentInstrument->freqRate = freqToFreqRate(tempFreq);
-	currentInstrument->period = freqToPeriod(tempFreq);
+	currentInstrument->position = 0;
 
 	if (DEBUG) {
-		printf("Generated Instrument:\nfreq: %f\nstart_beat: %d\nend_beat: %d\n", tempFreq, currentInstrument->start_beat, currentInstrument->end_beat);
+		printf("Generated Instrument:\nnote: %s\nfreq: %f\nstart_beat: %d\nend_beat: %d\n", currentInstrument->tone->name, currentInstrument->tone->freq, currentInstrument->start_beat, currentInstrument->end_beat);
 	}
 }
 
 /* This function goes through the list of instruments and moves each one closer to now */
-void nextBeat(instrument* instruments) {
+void nextBeat(Instruments* instruments) {
 	int n;
-	instrument* currentInstrument;
-	for (n=0; n<NUM_INSTRUMENTS; n++) {
-		currentInstrument = instruments + n;
+	Instrument* currentInstrument;
+	for (n=0; n < instruments->num; n++) {
+		currentInstrument = instruments->instrument + n;
 		currentInstrument->start_beat--;
 		currentInstrument->end_beat--;
 	}
@@ -122,12 +120,12 @@ void nextBeat(instrument* instruments) {
 }
 
 /* This function returns whether or not an instrument is active (Making a sound this beat) */
-int isActive(instrument* instr) {
+int isActive(Instrument* instr) {
 	return (instr->start_beat <=0 && instr->end_beat>=0);
 }
 
 /* This function returns whether or not an instrument is finished playing */
-int isFinished(instrument* instr) {
+int isFinished(Instrument* instr) {
 	return (instr->end_beat<0);
 }
 
@@ -141,7 +139,7 @@ float fadeOutVolume(long currentPos, long totalPos) {
 }
 
 /* This function returns the appropriate volume for the given instrument */
-float currentVolume(instrument* currentInstrument, long currentPos, long totalPos) {
+float currentVolume(Instrument* currentInstrument, long currentPos, long totalPos) {
 	if (currentInstrument->start_beat == 0) {
 		/* We've just started on this beat, should be fading in */
 		if (DEBUG) {
@@ -159,26 +157,23 @@ float currentVolume(instrument* currentInstrument, long currentPos, long totalPo
 	}
 }
 
-long beat_position; /* This is where we are in a given beat. */
-long beat_length; /* This is how many 'ticks' a given beat is. */
-
 void populate(void* data, Uint8* stream, int len) {
 	int n;
 	int numActiveInstruments;
 	int tempVolume;
 	long tempTotal;
-	instrument* activeInstruments[NUM_INSTRUMENTS];
-	instrument* currentInstrument;
-	instrument* instruments = (instrument*)data;
+	Instrument* activeInstruments[NUM_INSTRUMENTS];
+	Instrument* currentInstrument;
+	GlobalData* globalData = (GlobalData*)data;
 
 	/* Figure out the active instruments this beat */
 	numActiveInstruments = 0;
-	for(n=0; n<NUM_INSTRUMENTS; n++) {
-		if (isActive(instruments + n)) {
+	for(n=0; n < globalData->instruments.num; n++) {
+		if (isActive(globalData->instruments.instrument + n)) {
 			if (DEBUG) {
 				puts("This is an Active Instrument");
 			}
-			activeInstruments[numActiveInstruments] = instruments+n;
+			activeInstruments[numActiveInstruments] = globalData->instruments.instrument + n;
 			numActiveInstruments++;
 		}
 	}
@@ -187,25 +182,25 @@ void populate(void* data, Uint8* stream, int len) {
 		tempTotal = 0;
 		for (n=0; n<numActiveInstruments; n++) {
 			currentInstrument = activeInstruments[n];
-			tempTotal += (currentVolume(currentInstrument, beat_position, beat_length) / numActiveInstruments) * sinf(currentInstrument->freqRate * currentInstrument->position);
+			tempTotal += (currentVolume(currentInstrument, globalData->beat_position, globalData->beat_length) / numActiveInstruments) * (currentInstrument->tone->sample[currentInstrument->position]);
 			currentInstrument->position++;
-			currentInstrument->position %= currentInstrument->period;
+			currentInstrument->position %= currentInstrument->tone->period;
 		}
 		*stream = (Uint8) (tempTotal + 127);
 		stream++;
 
-		beat_position++;
-		if (beat_position > beat_length) {
+		(globalData->beat_position)++;
+		if (globalData->beat_position > globalData->beat_length) {
 			/* This is the next beat */
-			nextBeat(instruments);
+			nextBeat(&(globalData->instruments));
 			numActiveInstruments = 0;
-			for (n=0; n<NUM_INSTRUMENTS; n++) {
-				currentInstrument = instruments + n;
+			for (n=0; n < globalData->instruments.num; n++) {
+				currentInstrument = globalData->instruments.instrument + n;
 				if (isFinished(currentInstrument)) {
 					if (DEBUG) {
 						puts("This is a Finished Instrument");
 					}
-					generateTone(currentInstrument);
+					nextNote(currentInstrument, &(globalData->tones));
 				}
 				if (isActive(currentInstrument)) {
 					if (DEBUG) {
@@ -218,19 +213,21 @@ void populate(void* data, Uint8* stream, int len) {
 			if (numActiveInstruments == 0) {
 				/* In this case we're about to play silence. Lame! */
 				/* All we have to do is make one of the tones play now instead */
-				instruments->end_beat -= instruments->start_beat;
-				instruments->start_beat = 0;
-				activeInstruments[0] = instruments;
+				globalData->instruments.instrument->end_beat -= globalData->instruments.instrument->start_beat;
+				globalData->instruments.instrument->start_beat = 0;
+				activeInstruments[0] = globalData->instruments.instrument;
 				numActiveInstruments = 1;
 			}
-			beat_position = 0;
+			globalData->beat_position = 0;
 		}
 	}
 }
 
+
+
 int main(int argc, char* argv[]) {
+	GlobalData data;
 	int i;
-	instrument instruments[NUM_INSTRUMENTS];
 	SDL_AudioSpec spec;
 
 	spec.freq = FREQUENCY;
@@ -238,20 +235,68 @@ int main(int argc, char* argv[]) {
 	spec.channels = 1;
 	spec.samples = SAMPLES;
 	spec.callback = (*populate);
-	spec.userdata = instruments;
+	spec.userdata = (&data);
 
 	if (SDL_OpenAudio(&spec, NULL) < 0) {
 		fprintf(stderr, "Failed to open audio: %s\n", SDL_GetError());
 		exit(1);
 	}
 	srandom(time());
-	beat_position = 0;
-	beat_length = FREQUENCY / (TEMPO / 60.0);
 
+	/* Generate all the tones */
+	data.tones.num = 37;
+	data.tones.tone = malloc(sizeof(Tone) * data.tones.num);
+	genTone(data.tones.tone + 0, 65.41, "C2");
+	genTone(data.tones.tone + 1, 69.30, "C#2/Db2");
+	genTone(data.tones.tone + 2, 73.42, "D2");
+	genTone(data.tones.tone + 3, 77.78, "D#2/Eb2");
+	genTone(data.tones.tone + 4, 82.41, "E2");
+	genTone(data.tones.tone + 5, 87.31, "F2");
+	genTone(data.tones.tone + 6, 92.50, "F#2/Gb2");
+	genTone(data.tones.tone + 7, 98.00, "G2");
+	genTone(data.tones.tone + 8, 103.83, "G#2/Ab2");
+	genTone(data.tones.tone + 9, 110.00, "A2");
+	genTone(data.tones.tone + 10, 116.54, "A#2/Bb2");
+	genTone(data.tones.tone + 11, 123.47, "B2");
+	genTone(data.tones.tone + 12, 130.81, "C3");
+	genTone(data.tones.tone + 13, 138.59, "C#3/Db3");
+	genTone(data.tones.tone + 14, 146.83, "D3");
+	genTone(data.tones.tone + 15, 155.56, "D#3/Eb3");
+	genTone(data.tones.tone + 16, 164.81, "E3");
+	genTone(data.tones.tone + 17, 174.61, "F3");
+	genTone(data.tones.tone + 18, 185.00, "F#3/Gb3");
+	genTone(data.tones.tone + 19, 196.00, "G3");
+	genTone(data.tones.tone + 20, 207.65, "G#3/Ab3");
+	genTone(data.tones.tone + 21, 220.00, "A3");
+	genTone(data.tones.tone + 22, 233.08, "A#3/Bb3");
+	genTone(data.tones.tone + 23, 246.94, "B3");
+	genTone(data.tones.tone + 24, 261.63, "C4");
+	genTone(data.tones.tone + 25, 277.18, "C#4/Db4");
+	genTone(data.tones.tone + 26, 293.66, "D4");
+	genTone(data.tones.tone + 27, 311.13, "D#4/Eb4");
+	genTone(data.tones.tone + 28, 329.63, "E4");
+	genTone(data.tones.tone + 29, 349.23, "F4");
+	genTone(data.tones.tone + 30, 369.99, "F#4/Gb4");
+	genTone(data.tones.tone + 31, 392.00, "G4");
+	genTone(data.tones.tone + 32, 415.30, "G#4/Ab4");
+	genTone(data.tones.tone + 33, 440.00, "A4");
+	genTone(data.tones.tone + 34, 466.16, "A#4/Bb4");
+	genTone(data.tones.tone + 35, 493.88, "B4");
+	genTone(data.tones.tone + 36, 523.25, "C5");
+
+	/* Generate all the instruments */
+	data.instruments.num = NUM_INSTRUMENTS;
+	data.instruments.instrument = malloc(sizeof(Instrument) * data.instruments.num);
 	/* Generate Each Instrument */
-	for(i=0; i<NUM_INSTRUMENTS; i++) {
-		generateTone(instruments + i);
+	for(i=0; i<data.instruments.num; i++) {
+		nextNote(data.instruments.instrument + i, &(data.tones));
 	}
+
+	/* Set myself to the beginnning of the beat */
+	data.beat_position = 0;
+	/* Compute the length of a beat at this tempo */
+	data.beat_length = FREQUENCY / (TEMPO / 60.0);
+
 
 	SDL_PauseAudio(0);
 	sleep(20);
